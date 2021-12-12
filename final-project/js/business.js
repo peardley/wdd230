2641db146ee76232b899c8ebc4b22dfa25394f1a
peardley <lea96007@byui.edu>
2021-12-11 23:16:36 -0700

    4th push

--- final-project/js/business.js
+++ final-project/js/business.js
@@ -6,26 +6,26 @@ fetch(requestURL)
   })
   
 .then(function (jsonObject) {
-    const prophets = jsonObject['business'];
+    const business = jsonObject['business'];
     console.table(jsonObject); 
 
-    for (let i = 0; i < prophets.length; i++ ) {
+    for (let i = 0; i < business.length; i++ ) {
         let card = document.createElement('section');
         let img = document.createElement('img');
-        let h2 = document.createElement('h2');
+        let h3 = document.createElement('h3');
         let address = document.createElement('p');
         let phone = document.createElement('p');
         let website = document.createElement('p');
         
         img.setAttribute('src', business[i].logo);
         img.setAttribute('alt', `${business[i].name} logo`);
-        h2.textContent = `${business[i].name}`;
+        h3.textContent = `${business[i].name}`;
         address.textContent = `${business[i].address}`;
         phone.textContent = `${business[i].phonenumber}`;
         website.textContent = `${business[i].website}`;
 
         card.appendChild(img);
-        card.appendChild(h2);
+        card.appendChild(h3);
         card.appendChild(address);
         card.appendChild(phone);
         card.appendChild(website);
